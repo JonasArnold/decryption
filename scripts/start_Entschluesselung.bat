87cc667544ed48309c0ad40a348136bc89f380a3
Jonas Arnold <jonas.arnold@live.de>
2016-08-05 11:22:11 +0200

    DECR: Fixed Problem only .BMP files possible
    added support for .bmp files

--- scripts/start_Entschluesselung.bat
+++ scripts/start_Entschluesselung.bat
@@ -5,7 +5,8 @@ SET Pfad=%1
 SET Dateiendung=%~x1
 
 REM Starting the Decryption (Detecting the Filetype)
-if %Dateiendung% == .BMP (goto GOOD_FILETYPE) Else goto BAD_FILETYPE
+if %Dateiendung% == .BMP (goto GOOD_FILETYPE)
+if %Dateiendung% == .bmp (goto GOOD_FILETYPE) Else goto BAD_FILETYPE
 
 :BAD_FILETYPE
 MSG * The file-type "%Dateiendung%" does not work with DECRYPTION. To decrypt you need to use a ".BMP"-file!
